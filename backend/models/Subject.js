22720d15d77273cf432680af85c626af375bf74a
Vinay kumar <vinayp2702@gmail.com>
2025-03-25 19:28:47 +0530

    Add frontend folder

--- backend/models/Subject.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const subjectSchema = new mongoose.Schema({
-  subjectCode: String,
-  subjectName: String,
-  lecturers: [String],
-});
-
-module.exports = mongoose.model("Subject", subjectSchema);
